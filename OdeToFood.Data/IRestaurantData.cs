703dbfc2fabe11ac076ad4b9cc24d8e0f07840f2
Mirza Adnan <jmirza7@outlook.com>
2021-06-05 15:25:07 +0600

    Adding Create to the Data Access Service

--- OdeToFood.Data/IRestaurantData.cs
+++ OdeToFood.Data/IRestaurantData.cs
@@ -9,6 +9,7 @@ namespace OdeToFood.Data
         IEnumerable<Restaurant> GetRestaurantsByName(string name);
         Restaurant GetById(int id);
         Restaurant Update(Restaurant updatedRestaurant);
+        Restaurant Add(Restaurant newRestaurant);
         int Commit();
     }
 
@@ -50,6 +51,13 @@ namespace OdeToFood.Data
             }
 
             return restaurant;
+        }
+
+        public Restaurant Add(Restaurant newRestaurant)
+        {
+            restaurants.Add(newRestaurant);
+            newRestaurant.Id = restaurants.Max(r => r.Id) +1;
+            return newRestaurant;
         }
 
         public int Commit()
